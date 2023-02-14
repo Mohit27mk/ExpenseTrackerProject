4b008013c1dc26dde07a04a05b5dc913dbe16aac
Mohit27mk <114071726+Mohit27mk@users.noreply.github.com>
2023-02-14 23:37:24 +0530

    login frontend done

---
+++ Frontend/login.js
@@ -0,0 +1,31 @@
+const signupForm=document.querySelector('#login-form');
+const emailInput = document.querySelector('#email');
+const passwordInput = document.querySelector('#password');
+
+signupForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  
+  const email=e.target.email.value;
+  const password=e.target.password.value;
+
+    const myobj={
+      email,password
+    }
+    
+    axios.post("http://localhost:3000/user/login",myobj)
+    .then((res)=>{
+      if(res.data.Email==='notExist'){
+        alert("Create Account");
+      }else if(res.data.Password=='wrong'){
+        alert("wrong credentials");
+      }
+    }).catch((err)=>{
+     console.log(err);
+    })
+   emailInput.value='';
+   passwordInput.value='';
+    
+   
+}
